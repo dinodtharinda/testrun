d4a7a95008250c1af822375d96c6dc1127c78a3f
dinodtharinda <94352564+dinodtharinda@users.noreply.github.com>
2022-11-01 23:45:32 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:test_run/view/login_view.dart';
+import 'firebase_options.dart';
+import 'view/register_view.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MaterialApp(
+    title: 'Flutter Demo',
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final TextEditingController _email;
+
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified??false) {
+                print('you ara a verified user');
+              } else {
+                print('you need to verify your email first');
+              }
+              return Text(FirebaseAuth.instance.currentUser!.email.toString());
+            default:
+              return Column(
+                children: const [
+                  CircularProgressIndicator(),
+                  Text('Lodding...'),
+                ],
+              );
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ lib/view/register_view.dart
@@ -0,0 +1,189 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../firebase_options.dart';
+
+class RegisterView extends StatefulWidget {
+  const RegisterView({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Regiser'),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    decoration: BoxDecoration(
+                        border: Border.all(width: 1),
+                        borderRadius: BorderRadius.circular(15)),
+                    child: TextField(
+                      controller: _email,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      style: const TextStyle(
+                        fontSize: 20,
+                      ),
+                      decoration: const InputDecoration(
+                        hintText: 'Enter Your email',
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    decoration: BoxDecoration(
+                        border: Border.all(width: 1),
+                        borderRadius: BorderRadius.circular(15)),
+                    child: TextField(
+                        obscureText: true,
+                        enableSuggestions: false,
+                        autocorrect: false,
+                        keyboardType: TextInputType.emailAddress,
+                        controller: _password,
+                        style: const TextStyle(
+                          fontSize: 20,
+                        ),
+                        decoration: const InputDecoration(
+                            hintText: 'Enter Your Password',
+                            border: InputBorder.none)),
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      final email = _email.text;
+                      final password = _password.text;
+                      try {
+                        await FirebaseAuth.instance
+                            .createUserWithEmailAndPassword(
+                                email: email, password: password);
+                           
+                        var snackbar = SnackBar(
+                            content: const Text('Registered'),
+                            elevation: 16,
+                            backgroundColor:
+                                const Color.fromARGB(255, 27, 171, 51),
+                            behavior: SnackBarBehavior.floating,
+                            margin: const EdgeInsets.all(10),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15)),
+                            duration: const Duration(seconds: 10),
+                            action: SnackBarAction(
+                              label: 'Dismiss',
+                              textColor: Colors.black,
+                              onPressed: () {
+                                ScaffoldMessenger.of(context)
+                                    .hideCurrentSnackBar();
+                              },
+                            ));
+                        ScaffoldMessenger.of(context).showSnackBar(snackbar);
+                      } on FirebaseAuthException catch (e) {
+                        var errorMsg = ' error';
+                        print(e.code);
+                        if (e.code == 'user-not-found') {
+                          errorMsg = 'User not found!';
+                        } else if (e.code == 'unknown') {
+                          errorMsg = 'Please enter Email and Password!';
+                        } else if (e.code == 'email-already-in-use') {
+                          errorMsg = 'This email is already in use!';
+                        } else if (e.code == 'invalid-email') {
+                          errorMsg = 'Invalid Email!';
+                        } else if (e.code == 'weak-password') {
+                          errorMsg = 'Weak password!';
+                        } else if (e.code == 'too-many-requests') {
+                          errorMsg = 'Please try again later!';
+                        } else if (e.code == 'network-request-failed') {
+                          errorMsg = 'Please Connect Network';
+                        } else {
+                          errorMsg = e.code;
+                        }
+                        var snackbar = SnackBar(
+                            content: Text(errorMsg),
+                            elevation: 16,
+                            backgroundColor:
+                                const Color.fromARGB(255, 234, 15, 15),
+                            behavior: SnackBarBehavior.floating,
+                            margin: const EdgeInsets.all(10),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15)),
+                            duration: const Duration(seconds: 10),
+                            action: SnackBarAction(
+                              label: 'Dismiss',
+                              textColor: Colors.black,
+                              onPressed: () {
+                                ScaffoldMessenger.of(context)
+                                    .hideCurrentSnackBar();
+                              },
+                            ));
+                        ScaffoldMessenger.of(context).showSnackBar(snackbar);
+                      } catch (e) {
+                        var snackbar = SnackBar(
+                            content: Text(e.runtimeType.toString()),
+                            elevation: 16,
+                            backgroundColor: Colors.blueGrey,
+                            behavior: SnackBarBehavior.floating,
+                            margin: const EdgeInsets.all(10),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(25)),
+                            duration: const Duration(seconds: 10),
+                            action: SnackBarAction(
+                              label: 'Dismiss',
+                              onPressed: () {
+                                ScaffoldMessenger.of(context)
+                                    .hideCurrentSnackBar();
+                              },
+                            ));
+                        ScaffoldMessenger.of(context).showSnackBar(snackbar);
+                      }
+                    },
+                    child: const Text('Register'),
+                  ),
+                ],
+              );
+            default:
+              return Column(
+                children: const [
+                  CircularProgressIndicator(),
+                  Text('Lodding...'),
+                ],
+              );
+          }
+        },
+      ),
+    );
+  }
+}
