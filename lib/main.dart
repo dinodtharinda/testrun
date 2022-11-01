c44e1ad2214a9c909c7d676c4c13fb7726849da6
dinodtharinda <94352564+dinodtharinda@users.noreply.github.com>
2022-11-02 00:12:25 +0530

    issue fix

--- lib/main.dart
+++ lib/main.dart
@@ -64,7 +64,7 @@ class _HomePageState extends State<HomePage> {
               return Column(
                 children: const [
                   CircularProgressIndicator(),
-                  Text('Lodding...1234'),
+                  Text('Lodding...12345'),
                 ],
               );
           }
